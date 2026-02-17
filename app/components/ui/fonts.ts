f467cb7b0fb5ccc83bb85da9ed1f3aa5c2e40b60
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2026-02-17 22:53:39 +0100

    feat(App): Add Font To App

---
+++ app/components/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Inter_Tight } from "next/font/google";
+
+export const interTitleFont = Inter({
+    variable: "--font-inter-title",
+    subsets: ["latin"],
+});
+
+export const interSansFont = Inter_Tight({
+    variable: "--font-inter-sans",
+    subsets: ["latin"]
+})
